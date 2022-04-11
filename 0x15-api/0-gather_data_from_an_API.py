9222d6d0340e31f5a0f7b27bd0972021830597e9
f-i-t-s-u-m <fibabirr@gmail.com>
2022-04-11 12:21:20 +0300

    fix 0 again

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -1,25 +1,27 @@
 #!/usr/bin/python3
 """ Gather data from an api """
 
-import json
-import requests
-from sys import argv
 
-tasks = []
-done_task = 0
-all_tasks = 0
+if __name__ == "__main__":
+    import requests
+    from sys import argv
 
+    tasks = []
+    done_task = 0
+    all_tasks = 0
 
-em = requests.get(f'https://jsonplaceholder.typicode.com/users/{argv[1]}')
-r = requests.get(f'https://jsonplaceholder.typicode.com/users/{argv[1]}/todos')
-for i in r.json():
-    if i['completed'] is True:
-        tasks.append(i['title'])
-        done_task += 1
+    em = requests.get(f'https://jsonplaceholder.typicode.com/users/{argv[1]}')
+    if len(em.json()) != 0:
+        r = requests.get('https://jsonplaceholder.typicode.com/users/{}/todos'
+                         .format(argv[1]))
+        for i in r.json():
+            if i['completed'] is True:
+                tasks.append(i['title'])
+                done_task += 1
 
-    all_tasks += 1
+            all_tasks += 1
 
-print('Employee {} is done with tasks({}/{})'
-      .format(em.json()['name'], done_task, all_tasks))
-for i in tasks:
-    print('\t '+i)
+        print('Employee {} is done with tasks({}/{})'
+              .format(em.json()['name'], done_task, all_tasks))
+        for i in tasks:
+            print('\t '+i)
