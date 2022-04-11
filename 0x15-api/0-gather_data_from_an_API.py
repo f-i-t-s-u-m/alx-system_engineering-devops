c2ffec19fcc6cea03a1713a6fbaf6f4cad957f82
f-i-t-s-u-m <fibabirr@gmail.com>
2022-04-11 12:10:37 +0300

    fix 0

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -13,14 +13,13 @@ all_tasks = 0
 em = requests.get(f'https://jsonplaceholder.typicode.com/users/{argv[1]}')
 r = requests.get(f'https://jsonplaceholder.typicode.com/users/{argv[1]}/todos')
 for i in r.json():
-    if i['completed'] == True:
+    if i['completed'] is True:
         tasks.append(i['title'])
         done_task += 1
-    
+
     all_tasks += 1
 
 print('Employee {} is done with tasks({}/{})'
-        .format(em.json()['name'], done_task, all_tasks))
+      .format(em.json()['name'], done_task, all_tasks))
 for i in tasks:
     print('\t '+i)
-
