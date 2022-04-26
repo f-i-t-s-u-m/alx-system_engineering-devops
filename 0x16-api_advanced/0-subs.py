cd57f8eb772ba3d0c731b90a713c19f360c049aa
f-i-t-s-u-m <fibabirr@gmail.com>
2022-04-26 19:49:14 +0300

    fix 0 1

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -11,7 +11,7 @@ def number_of_subscribers(subreddit):
     """
 
     base_url = f'https://www.reddit.com/r/{subreddit}/about.json'
-    header = {'User-agent': 'alx'}
+    header = {'User-Agent': 'alx'}
     req = requests.get(base_url, headers=header, allow_redirects=False)
     if req.status_code == 200:
         data = req.json().get('data')
