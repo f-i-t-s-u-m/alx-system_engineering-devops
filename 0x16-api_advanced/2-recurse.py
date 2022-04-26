4d07075da5d5e451ea5a980cc9fe41f5be843cd9
f-i-t-s-u-m <fibabirr@gmail.com>
2022-04-27 00:24:45 +0300

    fix 2

--- 0x16-api_advanced/2-recurse.py
+++ 0x16-api_advanced/2-recurse.py
@@ -5,7 +5,7 @@
 n = 0
 
 
-def recurse(subreddit, hot_list=[], after=None):
+def recurse(subreddit, hot_list=[], after=None, count=0):
     """ return number of subcribers
         * subreddit - get subreddit account
     """
@@ -13,17 +13,18 @@ def recurse(subreddit, hot_list=[], after=None):
 
     req = requests.get('https://www.reddit.com/r/{}/hot.json'
                        .format(subreddit),
-                       params={'limit':1, 'after':after},
+                       params={'count': count, 'after': after},
                        headers={'User-agent': 'fitsum'},
                        allow_redirects=False)
-    if req.status_code >= 300:
+    if req.status_code >= 400:
         return None
 
     if req.json()['data']['after']:
-        data = req.json()['data']['children'][0]
-        hot_list.append(data['data']['title'])
-        print(hot_list)
-        recurse(subreddit, hot_list, req.json()['data']['after'])
+        data = req.json()['data']
+        children = data['children']
+        new_list = [x['data']['title'] for x in children]
+        hot_list.append(new_list)
+
+        recurse(subreddit, hot_list, data['after'], data.get('count'))
     else:
         return hot_list
-
